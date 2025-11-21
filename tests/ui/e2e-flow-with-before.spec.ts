75c6cdea5c058ff3c398837ad1ac548871a0a8d4
vladimirovodenko <vladimir@tallinn-learning.ee>
2025-11-21 22:30:09 +0200

    refactor: rename authPage to loginPage for consistency in test files

--- tests/ui/e2e-flow-with-before.spec.ts
+++ tests/ui/e2e-flow-with-before.spec.ts
@@ -3,17 +3,17 @@ import { LoginPage } from '../pages/login-page'
 import { faker } from '@faker-js/faker/locale/ar'
 import { PASSWORD, USERNAME } from '../../config/env-data'
 
-let authPage: LoginPage
+let loginPage: LoginPage
 
 test.beforeEach(async ({ page }) => {
-  authPage = new LoginPage(page)
-  await authPage.open()
+  loginPage = new LoginPage(page)
+  await loginPage.open()
 })
 
 test('signIn button disabled when incorrect data inserted', async ({}) => {
-  await authPage.usernameField.fill(faker.lorem.word(2))
-  await authPage.passwordField.fill(faker.lorem.word(7))
-  await expect(authPage.signInButton).toBeDisabled()
+  await loginPage.usernameField.fill(faker.lorem.word(2))
+  await loginPage.passwordField.fill(faker.lorem.word(7))
+  await expect(loginPage.signInButton).toBeDisabled()
 })
 
 test('error message displayed when incorrect credentials used', async ({}) => {
@@ -21,7 +21,7 @@ test('error message displayed when incorrect credentials used', async ({}) => {
 })
 
 test('login with correct credentials and verify order creation page', async ({}) => {
-  const orderCreationPage = await authPage.signIn(USERNAME, PASSWORD)
+  const orderCreationPage = await loginPage.signIn(USERNAME, PASSWORD)
   await expect(orderCreationPage.statusButton).toBeVisible()
   // verify at least few elements on the order creation page
 })
